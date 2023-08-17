e6dd5054caec0de845f78e7fb3279292f06bdbf9
sumdeg06@gmail.com <sumdeg06@gmail.com>
2023-08-17 23:24:20 +0300

    Pagination added

--- WebApi/Extensions/ServicesExtensions.cs
+++ WebApi/Extensions/ServicesExtensions.cs
@@ -37,5 +37,17 @@ namespace WebApi.Extensions
             services.AddSingleton<LogFilterAttribute>();
 
         }
+
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                    builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .WithExposedHeaders("X-Pagination"));
+            });
+        }
     }
 }
