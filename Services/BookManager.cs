693b33447c5a73ae871710067464b894bb47d37b
sumdeg06@gmail.com <sumdeg06@gmail.com>
2023-08-18 00:03:48 +0300

    Filter added

--- Services/BookManager.cs
+++ Services/BookManager.cs
@@ -46,8 +46,11 @@ namespace Services
 
         public async Task<(IEnumerable<BookDto> books, MetaData metaData)> GetAllBooksAsync(BookParameters bookParameters,bool trackChanges)
         {
+            if(!bookParameters.ValidPriceRange)
+                throw new PriceOutOfRangeBadRequestException(); 
+
             var booksWithMetaData= await _manager.Book.GetAllBooksAsync(bookParameters,trackChanges);
-             var booksDto=_mapper.Map<IEnumerable<BookDto>>(booksWithMetaData);
+            var booksDto=_mapper.Map<IEnumerable<BookDto>>(booksWithMetaData);
             return (booksDto, booksWithMetaData.MetaData);
         }
 
